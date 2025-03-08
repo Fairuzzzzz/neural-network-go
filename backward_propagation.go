4ea2e8824432f233c8301591c81c5c9b9278da30
fairuz <fairuz.afnan@gmail.com>
2025-03-09 05:56:29 +0700

    implement forward and backward propagation functions for neural network

---
+++ backward_propagation.go
@@ -0,0 +1,37 @@
+package main
+
+import "gonum.org/v1/gonum/mat"
+
+func backwardPropagation(X, Y, Z, W, b, A *mat.Dense, learningRate float64) {
+	// Batch Size
+	m := float64(X.RawMatrix().Cols)
+
+	// Hitung dA
+	dA := mat.NewDense(A.RawMatrix().Rows, A.RawMatrix().Rows, nil)
+	dA.Sub(A, Y) // dA = A - Y
+
+	// Hitung dZ = dA * ReLU(derevative)(Z)
+	dZ := mat.NewDense(Z.RawMatrix().Rows, Z.RawMatrix().Cols, nil)
+	dZ.Apply(func(i, j int, v float64) float64 {
+		return dA.At(i, j) * reluDerevative(v)
+	}, Z)
+
+	// Hitung gradients
+	Xt := mat.DenseCopyOf(X.T())
+	dW := mat.NewDense(W.RawMatrix().Rows, W.RawMatrix().Cols, nil)
+	dW.Mul(dZ, Xt)
+	dW.Scale(1/m, dW)
+
+	db := mat.NewDense(b.RawMatrix().Rows, 1, nil)
+	for i := 0; i < dZ.RawMatrix().Rows; i++ {
+		sum := 0.0
+		for j := 0; j < dZ.RawMatrix().Cols; j++ {
+			sum += dZ.At(i, j)
+		}
+		db.Set(i, 0, sum/m)
+	}
+
+	// Update Weight dan Bias
+	W.Sub(W, mat.NewDense(W.RawMatrix().Rows, W.RawMatrix().Cols, W.RawMatrix().Data))
+	b.Sub(b, mat.NewDense(b.RawMatrix().Rows, b.RawMatrix().Cols, b.RawMatrix().Data))
+}
---
+++ forward_propagation.go
@@ -0,0 +1,17 @@
+package main
+
+import "gonum.org/v1/gonum/mat"
+
+func forwardPropagation(X, W, b *mat.Dense) *mat.Dense {
+	Z := mat.NewDense(X.RawMatrix().Rows, X.RawMatrix().Cols, nil)
+
+	Z.Mul(W, X)
+	Z.Add(Z, b)
+
+	// ReLU Activation
+	A := mat.NewDense(X.RawMatrix().Rows, X.RawMatrix().Cols, nil)
+	A.Apply(func(i, j int, v float64) float64 {
+		return relu(v)
+	}, Z)
+	return Z
+}
